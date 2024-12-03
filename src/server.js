a920445c6b820afc2daa9b3b0b028898635600fc
rifky <rifkysinaga222@gmail.com>
2024-12-03 23:32:51 +0700

    fix bug host value

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const server = Hapi.server({
   port: 5000,
-  host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+  host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
   routes: {
     cors: {
-      origin: ['*'],
+      origin: ["*"],
     },
   },
 });
