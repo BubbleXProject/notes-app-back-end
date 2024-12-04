bb930b5da3ada428037ee127eafd113888c87c47
Rifky Fahriza Sinaga <116672301+BubbleXProject@users.noreply.github.com>
2024-12-04 08:37:28 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+const Hapi = require("@hapi/hapi");
 const server = Hapi.server({
   port: 5000,
   host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
@@ -7,3 +8,9 @@ const server = Hapi.server({
     },
   },
 });
+
+await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
